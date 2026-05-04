07b0991019bd61b6c4fc3ada7a650beefae1a2e6
TayoVan <ksiunya.s@gmail.com>
2026-05-04 19:44:08 +0300

    Delete ArrayListKsysha directory

--- ArrayListKsysha/ArratListKsysha/src/MyList.java
+++
@@ -1,9 +0,0 @@
-public interface MyList {
-    void add(String element);
-    void add(int index, String element);
-    String get(int index);
-    String remove(int index);
-    void set(int index, String element);
-    int size();
-    void clear();
-}
